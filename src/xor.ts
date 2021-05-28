c697247e729f1f4fb4291010afae6d18db7ae5ab
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2021-05-28 18:52:37 +0530

    Added xor test and eslint

---
+++ src/xor.ts
@@ -0,0 +1,17 @@
+const bufferXor = function xor(a: Buffer, b: Buffer): Buffer {
+  const length = Math.max(a.length, b.length);
+  const buffer = Buffer.allocUnsafe(length);
+
+  for (let i = 0; i < length; ++i) {
+    buffer[i] = a[i] ^ b[i];
+  }
+
+  return buffer;
+};
+
+export const hexXor = (hex1: string, hex2: string): string => {
+  const hex1Buffer = Buffer.from(hex1, "hex");
+  const hex2Buffer = Buffer.from(hex2, "hex");
+  const xorBuffer = bufferXor(hex1Buffer, hex2Buffer);
+  return xorBuffer.toString("hex");
+};
--- test/set1.test.ts
+++ test/set1.test.ts
@@ -1,4 +1,5 @@
 import { hexToBase64 } from "../src/base64";
+import { hexXor } from "../src/xor";
 
 describe("hexToBase64 test", () => {
   it("Sample hex test", () => {
@@ -9,5 +10,16 @@ describe("hexToBase64 test", () => {
     ).toEqual(
       "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
     );
+  });
+});
+
+describe("hexXor test", () => {
+  it("Sample hex test", () => {
+    expect(
+      hexXor(
+        "1c0111001f010100061a024b53535009181c",
+        "686974207468652062756c6c277320657965"
+      )
+    ).toEqual("746865206b696420646f6e277420706c6179");
   });
 });
