4f6bec53332d97e6f29600e9bd1332f94d30d34f
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2021-06-08 02:59:30 +0530

    Added variable length support in cbc

--- src/cbc.ts
+++ src/cbc.ts
@@ -3,13 +3,18 @@ import { bufferXor } from "./xor";
 import { splitBuffer } from "./helper";
 import { pkcs7Padding } from "./pkcs7";
 
-export const cbcEncrypt = (buf: Buffer, key: Buffer, iv: Buffer): Buffer => {
-  const splited = splitBuffer(buf, 16);
+export const cbcEncrypt = (
+  buf: Buffer,
+  key: Buffer,
+  iv: Buffer,
+  length: number = 16
+): Buffer => {
+  const splited = splitBuffer(buf, length);
   let encrypted = Buffer.alloc(0);
-  let currentBlock = pkcs7Padding(iv, 16);
+  let currentBlock = pkcs7Padding(iv, length);
   const aesEcb = new aesjs.ModeOfOperation.ecb(key);
   for (const plaintext of splited) {
-    const paddedPlaintext = pkcs7Padding(plaintext, 16);
+    const paddedPlaintext = pkcs7Padding(plaintext, length);
     const blockXored = bufferXor(paddedPlaintext, currentBlock);
     const blockEncrypted = aesEcb.encrypt(blockXored);
     encrypted = Buffer.concat([encrypted, blockEncrypted]);
@@ -18,9 +23,14 @@ export const cbcEncrypt = (buf: Buffer, key: Buffer, iv: Buffer): Buffer => {
   return encrypted;
 };
 
-export const cbcDecrypt = (buf: Buffer, key: Buffer, iv: Buffer): Buffer => {
-  const splited = splitBuffer(buf, 16);
-  let currentBlock = pkcs7Padding(iv, 16);
+export const cbcDecrypt = (
+  buf: Buffer,
+  key: Buffer,
+  iv: Buffer,
+  length: number = 16
+): Buffer => {
+  const splited = splitBuffer(buf, length);
+  let currentBlock = pkcs7Padding(iv, length);
   const plaintexts: Array<Buffer> = [];
   const aesEcb = new aesjs.ModeOfOperation.ecb(key);
   for (const blockEncrypted of splited) {
@@ -31,7 +41,7 @@ export const cbcDecrypt = (buf: Buffer, key: Buffer, iv: Buffer): Buffer => {
   }
   let decrypted = Buffer.concat(plaintexts);
   const padding = decrypted[decrypted.length - 1];
-  if (padding < 16) {
+  if (padding < length) {
     decrypted = decrypted.subarray(0, decrypted.length - padding);
   }
   return decrypted;
